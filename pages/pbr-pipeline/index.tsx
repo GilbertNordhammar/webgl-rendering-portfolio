e3080d9de081df52813133276fc4cdad9cf664cb
GilbertNordhammar <gilbertnordhammar@gmail.com>
2022-05-29 16:08:01 +0200

    cleanin up code

--- pages/pbr-pipeline/index.tsx
+++ pages/pbr-pipeline/index.tsx
@@ -13,9 +13,23 @@ const MAX_POINT_LIGHTS = 4
 let g_Scene: THREE.Scene;
 let g_Camera: THREE.PerspectiveCamera;
 let g_Renderer: THREE.WebGLRenderer;
-let g_CurrentObject: THREE.Object3D
 
-let g_Materials: Array<PBRShaderMaterial> = []
+class RenderObject {
+  constructor(params: { sceneObject: THREE.Object3D, materials: Array<PBRShaderMaterial> }) {
+    this.sceneObject = params.sceneObject
+    this.materials = params.materials
+  }
+
+  sceneObject: THREE.Object3D
+  materials: Array<PBRShaderMaterial>
+}
+let g_CurrentRenderObject: RenderObject
+const setCurrentRenderObject = (renderObject: RenderObject) => {
+  if (g_CurrentRenderObject)
+    g_Scene.remove(g_CurrentRenderObject.sceneObject)
+  g_Scene.add(renderObject.sceneObject)
+  g_CurrentRenderObject = renderObject
+}
 
 const g_Sun = new THREE.DirectionalLight(0xFFFFFF, 1)
 const g_AmbientLight = new THREE.AmbientLight(0xFFFFFF, 0.2)
@@ -31,52 +45,6 @@ interface MeshAlternative {
   name: string,
   load: () => Promise<{ object: THREE.Object3D, materials: Array<PBRShaderMaterial> }>
 }
-
-// const g_Meshes = {
-//   sphere: {
-//     load: async () => {
-//       const pbrMaterial = new PBRShaderMaterial({
-//         vertexShader: g_VertexShader,
-//         fragmentShader: g_FragmentShader,
-//       })
-
-//       const materialName = "ocean-rock"
-
-//       // if (uploadTextures["lastMaterial"] == materialName)
-//       //   return;
-//       // uploadTextures["lastMaterial"] = materialName
-
-//       pbrMaterial.albedoMap = await loadTexture(`pbr/${materialName}/albedo.png`, true)
-//       pbrMaterial.metalnessMap = await loadTexture(`pbr/${materialName}/metallic.png`, true)
-//       pbrMaterial.normalMap = await loadTexture(`pbr/${materialName}/normal.png`, true)
-//       pbrMaterial.roughnessMap = await loadTexture(`pbr/${materialName}/roughness.png`, true)
-//       pbrMaterial.ambientOcclusionMap = await loadTexture(`pbr/${materialName}/ao.png`, false)
-
-//       let geometry = new THREE.SphereGeometry(10, 32, 32);
-//       geometry.computeTangents()
-//       const mesh = new THREE.Mesh(geometry, pbrMaterial);
-
-//       g_Scene.add(mesh);
-//       g_Materials.push(pbrMaterial)
-
-//       return { object: mesh, material: pbrMaterial }
-//     }
-//   },
-//   dogHouse: {
-//     load: async () => {
-//       const meshGroup = await loadMesh("meshes/dog_house/doghouse0908.gltf")
-//       const pbrMaterial = new PBRShaderMaterial({
-//         vertexShader: g_VertexShader,
-//         fragmentShader: g_FragmentShader,
-//       })
-
-//       addMeshes({ object: meshGroup, pbrMaterial, scale: new Vector3(20, 20, 20) })
-//       g_Materials.push(pbrMaterial)
-
-//       return { object: meshGroup, material: pbrMaterial }
-//     }
-//   }
-// }
 
 const g_Meshes: Array<MeshAlternative> = [
   {
@@ -103,9 +71,6 @@ const g_Meshes: Array<MeshAlternative> = [
       geometry.computeTangents()
       const mesh = new THREE.Mesh(geometry, pbrMaterial);
 
-      g_Scene.add(mesh);
-      // g_Materials.push(pbrMaterial)
-
       return { object: mesh, materials: [pbrMaterial] }
     }
   },
@@ -119,16 +84,11 @@ const g_Meshes: Array<MeshAlternative> = [
       })
       const outPBRMaterials: Array<PBRShaderMaterial> = []
 
-      // addMeshes({ object: meshGroup, pbrMaterial, scale: new Vector3(20, 20, 20) })
-      // g_Materials.push(pbrMaterial)
-      // return { object: meshGroup["scene"], material: pbrMaterial }
-
       modifyMeshes({ object: meshGroup, inPBRMaterial, outPBRMaterials, scale: new Vector3(20, 20, 20) })
       return { object: meshGroup["scene"], materials: outPBRMaterials }
     }
   }
 ]
-
 
 const g_PBRMaterialNames = ["ocean-rock", "rusted-metal"]
 const g_defaultPBRMaterialName = g_PBRMaterialNames[0]
@@ -149,58 +109,15 @@ const init = async () => {
   // Adding lights
   g_Scene.add(g_AmbientLight)
 
-  addLight(g_Sun, g_Scene, g_Materials)
+  addLight(g_Sun, g_Scene)
 
   const pointlight = new THREE.PointLight(0xFFFFFF, 1, 100)
   pointlight.position.set(12, 2, 0)
-  addLight(pointlight, g_Scene, g_Materials)
+  addLight(pointlight, g_Scene)
 
-  // Add sphere mesh
-  // let geometry = new THREE.SphereGeometry(10, 32, 32);
-  // geometry.computeTangents()
-  // g_Mesh = new THREE.Mesh(geometry, g_Material);
-  // g_Scene.add(g_Mesh);
-
-  // // Adding lights
-  // g_Scene.add(g_AmbientLight)
-
-  // addLight(g_Sun, g_Scene, g_Materials)
-
-  // const pointlight = new THREE.PointLight(0xFFFFFF, 1, 100)
-  // pointlight.position.set(12, 2, 0)
-  // addLight(pointlight, g_Scene, g_Materials)
-
-  // console.log(g_Material.uniforms)
-
-  // loadMesh("meshes/dog_house/doghouse0908.gltf").then(meshGroup => {
-  //   const pbrMaterial = new PBRShaderMaterial({
-  //     vertexShader: g_VertexShader,
-  //     fragmentShader: g_FragmentShader,
-  //   })
-
-  //   addMeshes({ object: meshGroup, pbrMaterial, scale: new Vector3(20, 20, 20) })
-  //   g_Materials.push(pbrMaterial)
-
-  //   // Add sphere mesh
-  //   let geometry = new THREE.SphereGeometry(10, 32, 32);
-  //   geometry.computeTangents()
-  //   g_Mesh = new THREE.Mesh(geometry, g_Material);
-  //   // g_Scene.add(g_Mesh);
-
-  //   // Adding lights
-  //   g_Scene.add(g_AmbientLight)
-
-  //   addLight(g_Sun, g_Scene, g_Materials)
-
-  //   const pointlight = new THREE.PointLight(0xFFFFFF, 1, 100)
-  //   pointlight.position.set(12, 2, 0)
-  //   addLight(pointlight, g_Scene, g_Materials)
-
-  //   console.log(g_Materials[1].uniforms)
-  // })
   const { object, materials } = await g_Meshes[0].load()
-  g_CurrentObject = object
-  g_Materials = materials
+  setCurrentRenderObject(new RenderObject({ sceneObject: object, materials }))
+  console.log(g_CurrentRenderObject)
 }
 
 const modifyMeshes = (params: { object: THREE.Object3D, inPBRMaterial: PBRShaderMaterial, outPBRMaterials?: Array<PBRShaderMaterial>, scale?: Vector3 }) => {
@@ -219,52 +136,21 @@ const modifyMeshes = (params: { object: THREE.Object3D, inPBRMaterial: PBRShader
     mesh.material = newMaterial
     if (params.outPBRMaterials)
       params.outPBRMaterials.push(newMaterial)
-
-    console.log("Material updated", newMaterial, inPBRMaterial)
   }
 
   const children = object["scene"] ? object["scene"].children : object.children
   children.forEach(object => modifyMeshes({ ...params, object }))
 }
 
-// const addMeshes = (params: { object: THREE.Object3D, pbrMaterial: PBRShaderMaterial, scale?: Vector3 }) => {
-//   const { object, pbrMaterial } = params
-
-//   if (params.object instanceof THREE.Mesh) {
-//     const mesh = params.object as THREE.Mesh
-//     mesh.geometry.computeTangents()
-//     if (params.scale)
-//       mesh.scale.set(params.scale.x, params.scale.y, params.scale.z)
-
-//     pbrMaterial.copyPBRProperties(mesh.material as THREE.MeshStandardMaterial)
-//     mesh.material = pbrMaterial
-//     g_Materials.push(pbrMaterial)
-
-//     console.log("Mesh loaded")
-
-//     g_Scene.add(mesh)
-//   }
-
-//   const children = object["scene"] ? object["scene"].children : object.children
-//   children.forEach(child => addMeshes({ ...params, object: child }))
-// }
-
 const addLight = (
   light: THREE.PointLight | THREE.DirectionalLight,
-  scene: THREE.Scene,
-  materials: THREE.ShaderMaterial[]
+  scene: THREE.Scene
 ) => {
   let tooManyLights = false;
 
   if (light instanceof THREE.PointLight) {
     if (g_Pointlights.length + 1 <= MAX_POINT_LIGHTS) {
       g_Pointlights.push(light)
-
-      materials.forEach(mat => {
-        if (!mat.uniforms.pointLightsCount)
-          mat.uniforms.pointLightsCount = { value: 0 }
-        mat.uniforms.pointLightsCount.value = g_Pointlights.length
-      })
 
       const geometry = new THREE.SphereGeometry(1, 8, 8);
       const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
@@ -281,12 +167,6 @@ const addLight = (
   else if (light instanceof THREE.DirectionalLight) {
     if (g_DirectionalLights.length + 1 <= MAX_DIRECTIONAL_LIGHTS) {
       g_DirectionalLights.push(light)
-
-      materials.forEach(mat => {
-        if (!mat.uniforms.directionalLightsCount)
-          mat.uniforms.directionalLightsCount = { value: 0 }
-        mat.uniforms.directionalLightsCount.value = g_DirectionalLights.length
-      })
     }
     else {
       tooManyLights = true;
@@ -328,7 +208,7 @@ const animation = (time) => {
     movePointLight(index, pos)
   })
 
-  g_Materials.forEach(material => {
+  g_CurrentRenderObject?.materials.forEach(material => {
     material.uniforms.viewMatrixInverse = { value: g_Camera.matrixWorld }
     material.uniforms.pointLightsCount.value = g_Pointlights.length
     material.uniforms.directionalLightsCount.value = g_DirectionalLights.length
@@ -362,7 +242,7 @@ export default function PBRPipeline() {
       const xDir = e.movementX > 0 ? -1 : 1;
       const yDir = e.movementY > 0 ? -1 : 1;
       const movement = new Vector2(xDir * 5, 0);
-      rotateAround(g_Camera, g_CurrentObject.position, new Vector3(0, 1, 0), movement)
+      rotateAround(g_Camera, g_CurrentRenderObject.sceneObject.position, new Vector3(0, 1, 0), movement)
     });
     window.addEventListener('mousedown', e => {
       if (altDown) {
@@ -383,10 +263,6 @@ export default function PBRPipeline() {
     })
   }, [])
 
-  const hej = [
-    { values: { k: "a", load: () => console.log("hej") }, label: 'One' },
-    { value: { k: "b", load: () => console.log("hej") }, label: 'Two', className: 'myOptionClassName' }]
-
   const page = <div>
     <div style={{ display: "flex", justifyContent: "space-evenly" }}>
       <Dropdown options={g_PBRMaterialNames} value={g_defaultPBRMaterialName} onChange={materialName => {
@@ -394,12 +270,10 @@ export default function PBRPipeline() {
       }} />
       <Dropdown
         options={g_Meshes.map((mesh, i) => ({ label: mesh.name, value: i.toString() }))}
+        value={g_Meshes[0].name}
         onChange={entry => {
           g_Meshes[Number(entry.value)].load().then(({ object, materials }) => {
-            g_Scene.remove(g_CurrentObject)
-            g_CurrentObject = object
-            g_Scene.add(g_CurrentObject)
-            g_Materials = materials
+            setCurrentRenderObject(new RenderObject({ sceneObject: object, materials }))
           })
         }}
       />
@@ -476,7 +350,7 @@ export default function PBRPipeline() {
         minValue={0}
         maxValue={1}
         onChange={value => {
-          g_Materials.forEach(material => {
+          g_CurrentRenderObject.materials.forEach(material => {
             material.metalnessScale = value.x
           })
 
@@ -491,8 +365,7 @@ export default function PBRPipeline() {
         minValue={0}
         maxValue={1}
         onChange={value => {
-          g_Materials.forEach(material => {
-            console.log("pre", value)
+          g_CurrentRenderObject.materials.forEach(material => {
             material.roughnessScale = value.x
           })
         }}
